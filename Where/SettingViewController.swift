2cdd6c16ff08df0a152f89d7e928fd2eb462679f
森部高昌 <takamasamoribe@gmail.com>
2021-08-03 17:58:13 +0900

    0803 新しい場所を設定する画面を作成した
    画面遷移は、まだ

---
+++ Where/SettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingViewController.swift
+//  Where
+//
+//  Created by 森部高昌 on 2021/08/03.
+//
+
+import UIKit
+
+class SettingViewController: ViewController {
+    
+    
+    @IBOutlet weak var destLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        destLabel.text = "筑波山"
+    
+    }
+    
+}
