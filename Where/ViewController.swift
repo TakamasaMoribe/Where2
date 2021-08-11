7fd74fe3a7ccab2529ab1de50b048bd3b04e103a
森部高昌 <takamasamoribe@gmail.com>
2021-08-11 12:07:51 +0900

    0811   7   初期画面からの画面遷移の変更
    セグエを使わずに、前処理をする。前回使ったときの追加データを消去することを考えている

--- Where/ViewController.swift
+++ Where/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    
     // Startボタン押下時の処理
+    
+    @IBAction func startButton(_ sender: Any) {
+         // ①storyboardのインスタンス取得
+         let storyboard: UIStoryboard = self.storyboard!
+         // ②遷移先ViewControllerのインスタンス取得
+         let nextView = storyboard.instantiateViewController(withIdentifier: "CurrentViewController") as! CurrentViewController
+         // ③画面遷移
+         self.present(nextView, animated: true, completion: nil)
+     }
+    
+    
+    // 設定ボタン押下時の処理
     @IBAction func goSetting(_ sender: Any) {
          // ①storyboardのインスタンス取得
          let storyboard: UIStoryboard = self.storyboard!
