37abca852d3464e40734a0235186d052eb1a02a9
森部高昌 <takamasamoribe@gmail.com>
2021-08-23 17:26:02 +0900

    0823  1    起動時に、履歴データを消去することをやってみる

--- Where/ViewController.swift
+++ Where/ViewController.swift
@@ -18,9 +18,10 @@ class ViewController: UIViewController {
     
     // Startボタン押下時の処理
     @IBAction func startButton(_ sender: Any) {
-        //前回のデータを消去する
-        UserDefaults.standard.removeObject(forKey: "mtLatitude")
-        UserDefaults.standard.removeObject(forKey: "mtLongitude")
+//前回のデータを消去する
+//        UserDefaults.standard.removeObject(forKey: "mtName") // [2]山名
+//        UserDefaults.standard.removeObject(forKey: "mtLatitude")
+//        UserDefaults.standard.removeObject(forKey: "mtLongitude")
 
         // ①storyboardのインスタンス取得
         let storyboard: UIStoryboard = self.storyboard!
