71fda9aa06aa798f87fd471597f85521934f8776
森部高昌 <takamasamoribe@gmail.com>
2021-08-31 17:39:52 +0900

    0831  4   カスタムクラスを作って、ボタンの装飾をやってみる

--- Where/ViewController.swift
+++ Where/ViewController.swift
@@ -17,16 +17,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lastTimeButton: UIButton!
     
-
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // ボタンの装飾
-            let rgba = UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0) // ボタン背景色設定
-            startButton.backgroundColor = rgba                                               // 背景色
-            startButton.layer.borderWidth = 0.5                                              // 枠線の幅
-            startButton.layer.borderColor = UIColor.black.cgColor                            // 枠線の色
-            startButton.layer.cornerRadius = 5.0                                             // 角丸のサイズ
-            startButton.setTitleColor(UIColor.white, for: UIControl.State.normal)             // タイトルの色
+//        // ボタンの装飾
+//            let rgba = UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0) // ボタン背景色設定
+//
+//            button.backgroundColor = rgba                                               // 背景色
+//            button.layer.borderWidth = 0.5                                              // 枠線の幅
+//            button.layer.borderColor = UIColor.black.cgColor                            // 枠線の色
+//            button.layer.cornerRadius = 5.0                                             // 角丸のサイズ
+//            button.setTitleColor(UIColor.white, for: UIControl.State.normal)             // タイトルの色
         
     }
     
