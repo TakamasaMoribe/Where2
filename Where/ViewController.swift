ae774b5fa097645792a56b3196717a2ff7c372de
森部高昌 <takamasamoribe@gmail.com>
2021-09-09 20:22:04 +0900

    0909  1   起動画面の変更
    設定ボタンを無くして、スタートボタンと前回と同じボタンの２つにした

--- Where/ViewController.swift
+++ Where/ViewController.swift
@@ -16,23 +16,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    
     // Startボタン押下時の処理
-    @IBAction func startButton(_ sender: Any) {
-        //前回のデータを消去する
-        UserDefaults.standard.removeObject(forKey: "mtName") // [2]山名
-        UserDefaults.standard.removeObject(forKey: "mtLatitude")
-        UserDefaults.standard.removeObject(forKey: "mtLongitude")
-        
-        // ①storyboardのインスタンス取得
-        let storyboard: UIStoryboard = self.storyboard!
-        // ②遷移先ViewControllerのインスタンス取得
-        let nextView = storyboard.instantiateViewController(withIdentifier: "CurrentViewController") as! CurrentViewController
-        // ③画面遷移
-        self.present(nextView, animated: true, completion: nil)
-     }
-    
-    
-    // 設定ボタン押下時の処理
     @IBAction func goSetting(_ sender: Any) {
         //前回のデータを消去する
         UserDefaults.standard.removeObject(forKey: "mtName") // [2]山名
