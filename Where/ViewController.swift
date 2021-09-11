c9a539e39f42c98f8901674951dadb1f531944ac
森部高昌 <takamasamoribe@gmail.com>
2021-09-12 08:19:18 +0900

    0912  2  緯度・経度が（０，０）に線を引くことを解消した
    （０，０）でなければ、線を引く　

--- Where/ViewController.swift
+++ Where/ViewController.swift
@@ -8,14 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-//    @IBOutlet weak var textView: UITextView!
  
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     
     // Startボタン押下時の処理
     @IBAction func goSetting(_ sender: Any) {
